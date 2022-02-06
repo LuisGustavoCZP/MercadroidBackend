aa01abc7d9f177495b1cfafa4a6bb0e2b4be575e
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-02-06 20:20:19 -0300

    m

--- index.js
+++ index.js
@@ -1,16 +1,67 @@
-const port = 3000;
+const port = 8000;
+const path = `${__dirname}/src`;
 
 const express = require("express");
 const app = express();
+
+app.use(express.static(path));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+const d = new Date ();
+const categories = [
+    {"id":0, "name":"Category A"},
+    {"id":1, "name":"Category B"},
+    {"id":2, "name":"Category C"},
+];
+
+const products = [
+    {"id":0, "name":"Produto A", "categoryID":0, "quantyType":"L"},
+    {"id":1, "name":"Produto B", "categoryID":0, "quantyType":"G"}
+];
+
+const wallet = {"money":"200", "currency":"R$"}
+
+const markets = [
+    {
+        itens:[
+            {"id":0, "productID":0, "price":5},
+            {"id":1, "productID":1, "price":6}
+        ],
+        cart:[
+            {"id":0, "productID":0, "quanty":1}
+        ]
+    }
+];
+
 app.get("/", (req, res) => 
 {
-    const temp = {"id":0, "time":15};
-    /* const data = JSON.stringify(temp);
-    console.log(data); */
-    res.json(temp);
+    console.log(req.query);
+    res.json({products:products});
+});
+
+app.get("/categories", (req, res) => 
+{
+    console.log(req.query);
+    res.json({categories:categories});
+});
+
+app.get("/products", (req, res) => 
+{
+    console.log(req.query);
+    res.json({products:products});
+});
+
+app.get("/wallet", (req, res) => 
+{
+    console.log(req.query);
+    res.json(wallet);
+});
+
+app.get("/markets", (req, res) => 
+{
+    console.log(req.query);
+    res.json({markets:markets});
 });
 
 app.listen(port, () => {console.log(`Servidor iniciado em ${port}`)})
