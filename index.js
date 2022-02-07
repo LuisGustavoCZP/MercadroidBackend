eb2b38c274f76d063415bb2eec70d80e1be768a1
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-02-07 18:31:07 -0300

    m

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const port = 8000;
 const path = __dirname;
 
 const express = require("express");
-const { redirect } = require("express/lib/response");
+const fs = require("fs");
+//const { redirect } = require("express/lib/response");
 const cors = require('cors');
 const app = express();
 
@@ -11,11 +12,43 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
+/* let allowCrossDomain = function(req, res, next) 
+{
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', "*");
+    next();
+}
+app.use(allowCrossDomain); */
+
+let users = LoadUsers();
 const userSessions = [];
-userSessions["127.0.0.1"] = { "ip":"127.0.0.1", "id":0 };
-userSessions["::1"] = { "ip":"::1", "id":0 };
+//userSessions["127.0.0.1"] = { "ip":"127.0.0.1", "id":0 };
+//userSessions["::1"] = { "ip":"::1", "id":0 };
 
 const dateToday = new Date ();
+
+function LoadUsers () 
+{
+    const lusers = [];
+    const files = fs.readdirSync("data/users");
+    console.log(`Loaded ${files.length} users`);
+    files.forEach(f => 
+    {
+        const user = JSON.parse(fs.readFileSync("data/users/"+f));
+        lusers[user.id] = user;
+        //console.log(`User: ${user}`);
+    });
+    return lusers;
+}
+
+function SearchUser (username)
+{
+    for(id in users)
+    {
+        const user = users[id];
+        if(user.login == username) return id;
+    }
+}
 
 app.get("/", (req, res) => 
 {
@@ -30,11 +63,36 @@ app.get("/", (req, res) =>
     //res.json({"r":[]});
 });
 
-app.options('/user', cors())
+app.options('/user', cors());
 app.post("/user", (req, res) => 
 {
-    console.log(`Conexão ${req.ip} direcionado para area de usuario e ${req.body}`);
+    const username = req.body["user"], password = req.body["pass"];
+    if(username == "") 
+    {
+        console.log(`Conexão ${req.ip} sem username!`);
+        res.send(`-3`);
+        return;
+    }
+    if(password == "") 
+    {
+        console.log(`Conexão ${req.ip} sem password!`);
+        res.send(`-2`);
+        return;
+    }
+
+    const userid = SearchUser(username);
+    if(password != users[userid].password) 
+    {
+        console.log(`Conexão ${req.ip} password errado!`);
+        res.send(`-1`);
+        return;
+    }
+
+    console.log(`Conexão ${req.ip} direcionado para area de usuario e ${username}`);
+    const session = {"ip":`${req.ip}`, "userid":userid};
+    userSessions[session.ip] = session;
     
+    res.send(`${session.userid}`);
     //res.json({"r":[]});
 });
 
